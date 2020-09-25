12f3bfc75dba5df5f26be6f4afd395c32618af0f
jefe <jefe@gmail.com>
2020-09-25 12:58:36 +0200

    version 0

---
+++ src/Welcome.java
@@ -0,0 +1,10 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			 //estoy es un comentario
+			    System.out.println("Welcome to Java!");
+	}
+
+}
