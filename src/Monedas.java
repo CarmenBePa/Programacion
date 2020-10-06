a321d7113e36a6f3f184c92f3d2727cb5fe03525
jefe <jefe@gmail.com>
2020-10-06 13:16:11 +0200

    correccion tarea

--- src/Monedas.java
+++ src/Monedas.java
@@ -8,35 +8,36 @@ public class Monedas {
 
 		System.out.println("Introducir cantidad total de dinero a contar: ");
 		double cantidad = input.nextDouble();
-		int total = (int)(cantidad*100);
+		int total = (int)(cantidad);
 		
 		
 		int euros2 = total / 200;
-		int restEuros2 = euros2 % 200;
+		 total = total % 200;
 		
-		int euros1 = restEuros2 / 100;
-		int restEuros1 = restEuros2 % 100;
+		int euros1 = total / 100;
+		 total = total % 100;
 		
-		int cent50 = restEuros1 / 50;
-		int restCent50 = restEuros1 % 50;
+		int cent50 = total / 50;
+		 total = total % 50;
 		
-		int cent20 = restCent50 / 20;
-		int restCent20 = restCent50 % 20;
+		int cent20 = total / 20;
+		 total = total % 20;
 		
-		int cent10 = restCent20 / 10;
-		int restCent10 = restCent20 % 10;
+		int cent10 = total / 10;
+		 total = total % 10;
 		
-		int cent5 = restCent10 / 5;
-		int restCent5 = restCent10 % 5;
+		int cent5 = total / 5;
+		 total = total % 5;
 		
-		int cent2 = restCent5 / 2;
-		int restCent2 = restCent5 % 2;
+		int cent2 = total / 2;
+		 total = total % 2;
 		
-		int cent1 = restCent2 / 1;
-		int restCent1 = restCent2 % 1;
+		int cent1 = total / 1;
+		 total = total % 1;
 		
 		
-		System.out.println("En " + total + " centimos hay: ");
+		System.out.println("En " + cantidad + " centimos hay: ");
+		System.out.println("");
 		System.out.println( euros2 + " monedas de 2€");
 		System.out.println( euros1 + " monedas de 1€");
 		System.out.println( cent50 + " monedas de 50 centimos");
