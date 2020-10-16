3419db9bfa6654a6b7e203e6dacc7f3d5835d681
jefe <jefe@gmail.com>
2020-10-16 23:09:18 +0200

    tarea

--- src/Ecuacion.java
+++ src/Ecuacion.java
@@ -14,17 +14,18 @@ public class Ecuacion {
 		System.out.println("Introduce un valor para el tercer termino: ");
 		int c = input.nextInt();
 		
-		double dis = Math.pow(b, 2) - 4 * a * c;
+		double res = Math.pow(b, 2) - 4 * a * c;
 		
-		if (dis < 0) {
+		if (res < 0) {
 			System.out.println("La ecuacion no tiene solucion");
 		}
-		else if (dis == 0) {
-			System.out.println("La ecuacion tiene una unica solucion: " + (-b/2*a));
+		else if (res == 0) {
+			System.out.println("La ecuacion tiene una unica solucion: " + (-b/(2*a)));
 		}
 		else {
-			double x1 = (-b) + (Math.sqrt(dis)/2);
-			double x2 = (-b) - (Math.sqrt(dis)/2);
+			double x1 = (-b) + (Math.sqrt(res)/(2*a));
+			double x2 = (-b) - (Math.sqrt(res)/(2*a));
+			System.out.println("La ecuacion tiene dos soluciones. x="+x1 + " y x="+x2 );
 		}
 		
 		
