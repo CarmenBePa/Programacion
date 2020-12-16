80c913fdca8e80c9c457e96c962b1a26c4eee4e8
jefe <jefe@gmail.com>
2020-12-16 09:53:09 +0100

    clase

---
+++ src/trabajo_navidades/primerEjercicio.java
@@ -0,0 +1,25 @@
+package trabajo_navidades;
+
+import java.util.Scanner;
+
+public class primerEjercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Calcularemos los coeficientes de una ecuacion de 2º grado.");
+		
+		System.out.println("Introduce valor de a: ");
+		double a = input.nextDouble();		
+		System.out.println("Introduce valor de b: ");
+		double b = input.nextDouble();
+		System.out.println("Introduce valor de c: ");
+		double c = input.nextDouble();
+		
+		
+		//vamos a calcular ahora el valor del
+		
+	}
+
+}
